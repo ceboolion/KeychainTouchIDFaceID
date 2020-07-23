6c62c90383d44bc3a28fcc9e1c3401cd8cdac182
Roman Cebula <ceboolion@gmail.com>
2020-07-23 22:23:57 +0200

    add biometric authentication

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Ceboolion. All rights reserved.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -23,7 +24,28 @@ class ViewController: UIViewController {
 
 
     @IBAction func authenticateTapped(_ sender: UIButton) {
-        unlockSecretMessage()
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {[weak self] sucess, authenticsationError in
+                DispatchQueue.main.async {
+                    if sucess {
+                    self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentocation failed", message: "You could not be verified, please try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unvailable", message: "Your device is not configured for biometric authentication", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
     }
     
     @objc func adjustForKeyboard(notification: Notification){
